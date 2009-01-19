46d6434934b35568b82e2ea1a5127958bcb5ccda
Sebastien Grosjean <public@zencocoon.com>
2009-01-19 20:04:06 +0100

    Update to RSpec 1.1.12 and mocha

--- lib/spec/rails/matchers/validations.rb
+++ lib/spec/rails/matchers/validations.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module Matchers
       def validate_presence_of(attribute)
-        return simple_matcher("model to validate the presence of #{attribute}") do |model|
+        return simple_matcher("validates the presence of #{attribute}") do |model|
           model.send("#{attribute}=", nil)
           !model.valid? && model.errors.invalid?(attribute)
         end
@@ -21,7 +21,7 @@ module Spec
           max = options[:maximum]
         end
         
-        return simple_matcher("model to validate the length of #{attribute} within #{min || 0} and #{max || 'Infinity'}") do |model|
+        return simple_matcher("validates the length of #{attribute} within #{min || 0} and #{max || 'Infinity'}") do |model|
           invalid = false
           if !min.nil? && min >= 1
             model.send("#{attribute}=", 'a' * (min - 1))
@@ -39,14 +39,14 @@ module Spec
       end
 
       def validate_uniqueness_of(attribute)
-        return simple_matcher("model to validate the uniqueness of #{attribute}") do |model|
-          model.class.stub!(:find).and_return(true)
+        return simple_matcher("validates the uniqueness of #{attribute}") do |model|
+          model.class.stubs(:find).returns(true)
           !model.valid? && model.errors.invalid?(attribute)
         end
       end
 
       def validate_confirmation_of(attribute)
-        return simple_matcher("model to validate the confirmation of #{attribute}") do |model|
+        return simple_matcher("validates the confirmation of #{attribute}") do |model|
           model.send("#{attribute}_confirmation=", 'asdf')
           !model.valid? && model.errors.invalid?(attribute)
         end
